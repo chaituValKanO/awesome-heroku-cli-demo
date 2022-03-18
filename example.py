939b79ad651cf05d889f59898c952559f7a9a6b0
chaituValKanO <chaitu0406@gmail.com>
2022-03-18 11:57:44 +0530

    Added version to JSON output to test CI/CD

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def get_items(item_id: int, count: int = 1):
-    return {"fetch": f"Fetched {count} of {item_id}"}
+    return {"App_Version: 1.1.0 - fetch": f"Fetched {count} of {item_id}"}
