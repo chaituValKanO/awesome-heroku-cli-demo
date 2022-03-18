38c75d484cbd1b802502fe5e3f8fd220813f739b
chaituValKanO <chaitu0406@gmail.com>
2022-03-18 12:01:16 +0530

    updated test_example.py and requirements.txt for CI/CD test

--- test_example.py
+++ test_example.py
@@ -6,13 +6,13 @@ test_agent = TestClient(app)
 def test_path():
     res = test_agent.get("/items/42")
     assert res.status_code == 200
-    assert res.json() == {"fetch": "Fetched 1 of 42"}
+    assert res.json() == {"App_Version: 1.1.0 - fetch": "Fetched 1 of 42"}
     
 
 def test_get_path_query():
     r = test_agent.get("/items/42?count=5")
     assert r.status_code == 200
-    assert r.json() == {"fetch": "Fetched 5 of 42"}
+    assert r.json() == {"App_Version: 1.1.0 - fetch": "Fetched 5 of 42"}
 
 
 def test_get_malformed():
